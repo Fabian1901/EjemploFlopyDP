0d2d0a41e0fe06283c302f383b4b80741844930a
Carlos Fabian Angarita C <carlos2180185@correo.uis.edu.co>
2021-01-18 17:34:48 -0500

    Mi primer commit de modelado

---
+++ clase1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 18 16:20:19 2021
+
+@author: Carlos Fabian Anagarita
+"""
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import flopy
