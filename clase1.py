8688bab96759b5defb93add020e5ed5599b6da31
Carlos Fabian Angarita C <carlos2180185@correo.uis.edu.co>
2021-01-22 14:05:29 -0500

    Anadimos paquete temporal, solucionador y groundmodel flower

--- clase1.py
+++ clase1.py
@@ -10,6 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import flopy
 
+#Se ingresan los datos 
 name = "tutorial01_mf6"
 h1 = 100
 h2 = 90
@@ -19,6 +20,29 @@ L = 400.0
 H = 50.0
 k = 1.0
 
+#Busca mf6 en la carpeta seleccionada y guarda archivos
 sim = flopy.mf6.MFSimulation(
-    sim_name=name, exe_name="mf6", version="mf6", sim_ws="."
+    sim_name=name, exe_name="C:/Users/Owner/Desktop/Universidad/DIPLOMADO GEOCIENCIA/mf6.2.0/bin/mf6",
+    version="mf6", sim_ws="WorkSpace"
 )
+
+#Create the Flopy TDIS object
+tdis = flopy.mf6.ModflowTdis(
+    sim, pname="tdis", time_units="DAYS", nper=1, perioddata=[(1.0, 1, 1.0)])
+
+#Create the Flopy IMS Package object# # IMS es el solver
+ims = flopy.mf6.ModflowIms(sim, pname="ims", complexity="SIMPLE")
+
+#Create the Flopy groundwater flow (gwf) model object
+model_nam_file = "{}.nam".format(name)
+gwf = flopy.mf6.ModflowGwf(sim, modelname=name, model_nam_file=model_nam_file)
+
+
+
+
+
+
+
+
+
+
