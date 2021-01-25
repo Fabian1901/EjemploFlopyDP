7bef13beaefa7899e27c59790b1f3d19ef498647
Carlos Fabian Angarita C <carlos2180185@correo.uis.edu.co>
2021-01-25 18:00:59 -0500

    Se cambió la permeabilidad a una capa.

--- clase1.py
+++ clase1.py
@@ -56,6 +56,8 @@ start = h1 * np.ones((Nlay, N, N))
 ic = flopy.mf6.ModflowGwfic(gwf, pname="ic", strt=start) #Initial conditions.
 
 #Not property flow - Controla flujo entre celdas.
+k=np.ones([10,N,N])
+k[1,:,:]=5e-3
 npf = flopy.mf6.ModflowGwfnpf(gwf, icelltype=1, k=k, save_flows=True)
 
 #
